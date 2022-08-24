32e887ca6d71e88d5f580b41c2db149598155818
Renan Schotten <renan.schotten@gmail.com>
2022-08-24 11:42:33 -0300

    h2 database, test profile, jpa

--- src/main/java/com/example/course/entities/Users.java
+++ src/main/java/com/example/course/entities/Users.java
@@ -3,21 +3,29 @@ package com.example.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Users implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 
-	public User() {
+	public Users() {
 		super();
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public Users(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -79,7 +87,7 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Users other = (Users) obj;
 		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
 				&& Objects.equals(password, other.password) && Objects.equals(phone, other.phone);
 	}
--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.course.entities.User;
+import com.example.course.entities.Users;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99595", "123456");
+	public ResponseEntity<Users> findAll() {
+		Users u = new Users(1L, "Maria", "maria@gmail.com", "99595", "123456");
 		return ResponseEntity.ok().body(u);
 	}
 }
